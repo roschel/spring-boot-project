c6fbfa241f6a31ffd89523ece154396961b5c463
roschel <joaomroschel@gmail.com>
2020-10-01 08:29:27 -0300

    consuming cep public api

--- src/main/java/curso/api/rest/controller/IndexController.java
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -5,9 +5,17 @@
  */
 package curso.api.rest.controller;
 
+import com.google.gson.Gson;
 import curso.api.rest.model.Usuario;
 import curso.api.rest.model.UsuarioDTO;
 import curso.api.rest.repository.UsuarioRepository;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -66,12 +74,41 @@ public class IndexController {
     }
     
     @PostMapping(value="/", produces="application/json")
-    public ResponseEntity<Usuario> post(@RequestBody Usuario usuario) {
+    public ResponseEntity<Usuario> post(@RequestBody Usuario usuario) throws Exception {
         
         //Associando telefones ao objeto pai usuario
         for (int pos = 0;pos<usuario.getTelefones().size();pos++){
             usuario.getTelefones().get(pos).setUsuario(usuario);
         }
+
+        /**
+         * Consumindo API externa de CEP
+         */
+        URL url = new URL(String.format("https://viacep.com.br/ws/%s/json/", usuario.getCep()));
+        URLConnection connection = url.openConnection();
+        InputStream is = connection.getInputStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+
+        String cep="";
+        StringBuilder jsonCep = new StringBuilder();
+
+        while ((cep = br.readLine()) != null){
+            jsonCep.append(cep);
+        }
+
+        // Convertendo string em json
+        Usuario userAux = new Gson().fromJson(jsonCep.toString(),Usuario.class);
+        usuario.setCep(userAux.getCep());
+        usuario.setLogradouro(userAux.getLogradouro());
+        usuario.setBairro(userAux.getBairro());
+        usuario.setComplemento(userAux.getComplemento());
+        usuario.setLocalidade(userAux.getLocalidade());
+        usuario.setUf(userAux.getUf());
+
+        /**
+         * Consumindo API externa de CEP
+         */
+
 
         String senhaCriptografada = new BCryptPasswordEncoder().encode(usuario.getSenha());
         usuario.setSenha(senhaCriptografada);
@@ -83,7 +120,7 @@ public class IndexController {
     }
     
     @PutMapping(value="/", produces="application/json")
-    public ResponseEntity<Usuario> put(@RequestBody Usuario usuario) {
+    public ResponseEntity<Usuario> put(@RequestBody Usuario usuario) throws Exception{
         
         //Associando telefones ao objeto pai usuario
         for (int pos = 0;pos<usuario.getTelefones().size();pos++){
@@ -101,6 +138,37 @@ public class IndexController {
                 //Caso senha seja um campo vazio, pegar a ultima senha cadastrada
                 usuario.setSenha(userTemporario.getSenha());
             }
+        }
+
+        if (usuario.getCep() != null) {
+            /**
+             * Consumindo API externa de CEP
+             */
+            URL url = new URL(String.format("https://viacep.com.br/ws/%s/json/", usuario.getCep()));
+            URLConnection connection = url.openConnection();
+            InputStream is = connection.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+
+            String cep="";
+            StringBuilder jsonCep = new StringBuilder();
+
+            while ((cep = br.readLine()) != null){
+                jsonCep.append(cep);
+            }
+
+            // Convertendo string em json
+            Usuario userAux = new Gson().fromJson(jsonCep.toString(),Usuario.class);
+            usuario.setCep(userAux.getCep());
+            usuario.setLogradouro(userAux.getLogradouro());
+            usuario.setBairro(userAux.getBairro());
+            usuario.setComplemento(userAux.getComplemento());
+            usuario.setLocalidade(userAux.getLocalidade());
+            usuario.setUf(userAux.getUf());
+
+            /**
+             * Consumindo API externa de CEP
+             */
+
         }
         
         Usuario usuarioSave = usuarioRepository.save(usuario);
